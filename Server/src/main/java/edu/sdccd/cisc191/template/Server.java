b18cd96aeeba30bd35c2b7ede7e853d1698ff16b
Christian Jimenez <111768883+ChristianSpencerJimenez@users.noreply.github.com>
2022-08-29 18:49:02 -0700

    Menu project idea module 1

--- Server/src/main/java/edu/sdccd/cisc191/template/Server.java
+++ Server/src/main/java/edu/sdccd/cisc191/template/Server.java
@@ -6,6 +6,7 @@ package edu.sdccd.cisc191.template;
 //import java.net.*;
 //import java.io.*;
 //import java.util.Random;
+import java.util.Queue;
 import java.util.Scanner;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -19,6 +20,7 @@ This is where I will write modules for the Arch. Project for CISC 191
 
 public class Server {
     public static void main(String[] args) {
+
         Scanner consoleInput = new Scanner(System.in);
 
         System.out.println("                                                                                 ");
@@ -39,22 +41,27 @@ public class Server {
         String[] dinnerType = {"breakfast", "lunch", "dinner"};
         System.out.println("Please type your name below:");
         System.out.println("==================================================================================================");
-        while (true) {
+        while (true)
+        {
 
             String userName = consoleInput.nextLine();
-            if (userName.length() < 2) {
+            if (userName.length() < 2)
+            {
                 System.out.println("==================================================================================================");
                 System.out.println("That name is to short!");
                 System.out.println("==================================================================================================");
                 continue;
-            } else if (!userName.matches("[a-zA-Z]+")) {
+            }
+            else if (!userName.matches("[a-zA-Z]+"))
+            {
                 System.out.println("==================================================================================================");
                 System.out.println("                 Error, please enter only a first name");
                 System.out.println("                  Special Characters are not allowed");
                 System.out.println("==================================================================================================");
                 continue;
-            } else {
-
+            }
+            else
+            {
                 /*
                  *
                  *  [begin second selection menu]
@@ -64,7 +71,6 @@ public class Server {
                  *  showing menu options based on breakfast, lunch, and dinner.
                  *
                  */
-
                 System.out.println("==================================================================================================");
                 System.out.println("                                   Hello " + userName + "!");
                 System.out.println("                             Our Food Options are... ");
@@ -75,49 +81,62 @@ public class Server {
                 System.out.println("                        [Enter one of the three options!]");
                 System.out.println("==================================================================================================");
             }
-            while (true) {
+            while (true)
+            {
                 String selectionType = consoleInput.next().toLowerCase();
                 if (selectionType.length() < 2) {
-                    System.out.println("==================================================================================================");
-                    System.out.println("          That's to short, type out 'Breakfast', 'Lunch', or 'Dinner'!");
-                    System.out.println("==================================================================================================");
+                         System.out.println("==================================================================================================");
+                         System.out.println("          That's to short, type out 'Breakfast', 'Lunch', or 'Dinner'!");
+                         System.out.println("==================================================================================================");
 
-                } else if (!selectionType.matches("[a-zA-Z]+")) {
-                    System.out.println("==================================================================================================");
-                    System.out.println("               That is not a valid type, please try again.");
-                    System.out.println("                       -You must only use letters.");
-                    System.out.println("==================================================================================================");
+                }
+                else if (!selectionType.matches("[a-zA-Z]+"))
+                {
+                        System.out.println("==================================================================================================");
+                        System.out.println("               That is not a valid type, please try again.");
+                        System.out.println("                       -You must only use letters.");
+                        System.out.println("==================================================================================================");
 
-                } else {
+                }
+                else
+                {
                     if (selectionType.matches(dinnerType[0])) {
                         System.out.println("==================================================================================================");
                         System.out.println("                           You chose Breakfast");
                         BreakfastMenu.breakfastMenuList();
                         break;
 
-                    } else if (selectionType.matches(dinnerType[1])) {
+                    }
+                    else if (selectionType.matches(dinnerType[1]))
+                    {
                         System.out.println("==================================================================================================");
                         System.out.println("                             You chose Lunch");
                         LunchMenu.lunchMenuList();
                         break;
 
-                    } else if (selectionType.matches(dinnerType[2])) {
+                    }
+                    else if (selectionType.matches(dinnerType[2]))
+                    {
                         System.out.println("==================================================================================================");
                         System.out.println("                             You chose Dinner");
                         DinnerMenu.dinnerMenuList();
                         break;
 
-                    } else {
+                    }
+                    else
+                    {
                         System.out.println("==================================================================================================");
                         System.out.println("                             Invalid Selection.");
                         System.out.println("        -Try typing a menu type like 'Breakfast', 'Lunch', or 'Dinner'!");
                         System.out.println("==================================================================================================");
                     }
                 }
-            }break;// This break prevents a looping back to the original while(true) loop in SERVER class.
+            }
+            break;// This break prevents a looping back to the original while(true) loop in SERVER class.
         }
     }
-} //end of SERVER class
+}
+    //end of SERVER class
 
 
     //The following code below are subclasses that assist
@@ -134,11 +153,15 @@ class FoodRecommendedTime extends Server {
             System.out.println("                        We recommend you look at our Breakfast menu!");
             System.out.println("==================================================================================================");
 
-        } else if (Time < 18) {
+        }
+        else if (Time < 18)
+        {
             System.out.println("                     Hello and good afternoon, this is our menu terminal.");
             System.out.println("                        We recommend you look at our Lunch menu!");
             System.out.println("==================================================================================================");
-        } else if (Time >= 18) {
+        }
+        else if (Time >= 18)
+        {
             System.out.println("                     Hello and good evening, this is our menu terminal.");
             System.out.println("                        We recommend you look at our Dinner menu!");
             System.out.println("==================================================================================================");
@@ -151,7 +174,7 @@ class FoodRecommendedTime extends Server {
         public static void breakfastMenuList() {
 
             Scanner consoleInput = new Scanner(System.in);
-           // String[] confirmSelection = {"yes", "no",};           ***YES NO SELECTION
+            // String[] confirmSelection = {"yes", "no",};           ***YES NO SELECTION USING STRING LITERALS
 
             System.out.println();
             System.out.println(":::::::::  :::::::::  ::::::::::     :::     :::    ::: ::::::::::   :::      :::::::: :::::::::::");
@@ -174,52 +197,94 @@ class FoodRecommendedTime extends Server {
             System.out.println("*==============================================*=================================================*");
 
 
-            //Adding the monetary values to the items on the menu for final price calculation.
-            double waffles = 5.00, eggsAndBacon = 6.00, cereal = 2.00;
-            double coffee = 3.25, milk = 1.25, orangeJuice = 2.35;
-            int selectedFoodInt = 0, selectedDrink = 0;
-            String[][] breakfastSelection;                                                     // THIS IS THE DOUBLE ARRAY, Will set values to appropriate choices in menu to send to final.
-            breakfastSelection = new String[2][2];                                             //USE THIS 2D ARRAY TO STORE DATA FOR FINAL SELECTION
+            double[][] foodSelectionTableCost = new double[3][3];
+            foodSelectionTableCost[0][0] = 8.25;  //PRICE OF (Waffles + Coffee) = TotalCost
+            foodSelectionTableCost[0][1] = 7.25;  //PRICE OF (Waffles + Milk)   = TotalCost
+            foodSelectionTableCost[0][2] = 7.35;  //PRICE OF (Waffles + OrangeJuice) = TotalCost
+            foodSelectionTableCost[1][0] = 9.25;  //PRICE OF (Eggs and Bacon + Coffee) = TotalCost
+            foodSelectionTableCost[1][1] = 7.25;  //PRICE OF (Eggs and Bacon + Milk)   = TotalCost
+            foodSelectionTableCost[1][2] = 8.25;  //PRICE OF (Eggs and Bacon + Orange Juice) = TotalCost
+            foodSelectionTableCost[2][0] = 5.25;  //PRICE OF (Cereal + Coffee) = TotalCost
+            foodSelectionTableCost[2][1] = 3.25;  //PRICE OF (Cereal + Milk)   = TotalCost
+            foodSelectionTableCost[2][2] = 4.35;  //Price OF (Cereal + OrangeJuice) = TotalCost
 
-            String[] menuFoodItemList;
-            menuFoodItemList = new String[]{"Waffles", "Eggs and Bacon", "Cereal"};
 
-            //Selection
+            String[] menuDisplayDrinkItemList = new String[]{"Coffee",  "Milk"          , "Orange Juice" };
+            String[] menuDisplayFoodItemList = new  String[]{"Waffles", "Eggs and Bacon", "Cereal"};
+
+            //Selection integers to be used later when using the above array.
+            int selectedFood = 0, selectedDrink = 0;
+
             System.out.println(" " +
                     "                                Please Select Your Food Option!\n" +
                     "                                 Enter the menu number below.");
 
             while (true) {
-                String selectionMenuType = consoleInput.next();
-                int selectedFood = 0;
-                if (selectionMenuType.length() > 1) {
+                String selectionMenuFoodType = consoleInput.next();
+                if (selectionMenuFoodType.length() > 1) {
                     System.out.println("please only enter a single number selection.");
 
-                } else if (!selectionMenuType.matches("[1-3]+")) {
+                } else if (!selectionMenuFoodType.matches("[1-3]+")) {                                          //Redundant way of domain restriction. Based on CH.2, however still works in this case.
                     System.out.println("Please enter a number selection between [1, 2, 3]");
 
-                } else
-                    selectedFood = Integer.parseInt(selectionMenuType);
-
-                if (selectedFood == 1) {
-                    System.out.println("you have selected menu item " +selectedFood + ", " + menuFoodItemList[0] + "\n");
-                    consoleInput.next();
-
-                    break;
-                } else if (selectedFood == 2) {
-                    System.out.println("you have selected menu item " +selectedFood + ", " + menuFoodItemList[1] + "\n");
-                    consoleInput.next();
-
-                    break;
-                } else if (selectedFood == 3) {
-
-                    System.out.println("you have selected menu item " + selectedFood + ", " + menuFoodItemList[2] + "\n");
-                    consoleInput.next();
-
+                }
+                else
+                    selectedFood = Integer.parseInt(selectionMenuFoodType);
+                    selectedFood -= 1; //SELECTED FOOD IS REDUCED BY ONE SO IT CAN BE READ CORRECTLY IN THE foodSelectionTableCost ARRAY.
+                if (selectedFood == 0)
+                {
+                    System.out.println("you have selected menu item " + (selectedFood+1) + ", " + menuDisplayFoodItemList[0] + "\n");
                     break;
                 }
-
+                else if (selectedFood == 1) {
+                    System.out.println("you have selected menu item " + (selectedFood+1) + ", " + menuDisplayFoodItemList[1] + "\n");
+                    break;
+                }
+                else if (selectedFood == 2) {
+                    System.out.println("you have selected menu item " + (selectedFood+1) + ", " + menuDisplayFoodItemList[2] + "\n");
+                    break;
+                }
             }
+            while (true)
+            {
+                String selectionMenuDrinkType = consoleInput.next();
+
+                if (selectionMenuDrinkType.length() > 1)
+                {
+                    System.out.println("please only enter a single number selection.");
+                }
+                else if (!selectionMenuDrinkType.matches("[4-6]+"))   //Redundant way of domain restriction. Based on CH.2, however still works in this case.
+                    System.out.println("Please enter a number selection between [4, 5, 6]");
+                else
+                    System.out.println(" " +
+                            "                                Please Select Your Drink Option!\n" +
+                            "                                 Enter the menu number below.");
+
+                    selectedDrink = Integer.parseInt(selectionMenuDrinkType);
+                    selectedDrink -= 1; //SELECTED FOOD IS REDUCED BY ONE SO IT CAN BE READ CORRECTLY IN THE foodSelectionTableCost ARRAY.
+                if (selectedDrink == 3)
+                {
+                    System.out.println("you have selected menu item " + (selectedDrink+1) + ", " + menuDisplayDrinkItemList[0] + "\n");
+                    break;
+                }
+                else if (selectedFood == 4) {
+                    System.out.println("you have selected menu item " + (selectedFood+1) + ", " + menuDisplayDrinkItemList[1] + "\n");
+                    break;
+                }
+                else if (selectedFood == 5) {
+                    System.out.println("you have selected menu item " + (selectedFood+1) + ", " + menuDisplayDrinkItemList[2] + "\n");
+                    break;
+
+
+
+
+
+                }
+
+
+
+
+                }
         }
     }
                //DRINK SELECTION AREA
@@ -265,11 +330,7 @@ class FoodRecommendedTime extends Server {
             }
         }
 
-        class ConfirmSelectionMenu extends Server {
-            public static void confirmSelectionMenu() {
-                System.out.println("test final");
-            }
-        }
+
 
 
 
